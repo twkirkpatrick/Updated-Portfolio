29b2596daf1da225aa92f7cf5562752675ac1216
Tanner Kirkpatrick <twk4491@gmail.com>
2020-10-08 16:11:53 -0400

    about page styling

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
     $("body").hide().append(
         "<div class='container'>" +
             "<div class='row'>" +
-                "<div class='col-sm-6 offset-sm-3 img-col'>" + 
+                "<div class='col-sm-6 offset-sm-3 img-col about-page'>" + 
                 "<img src = 'images/twk.jpg' class='about-img'>" +
                     "<div class='row'>" +
                         "<div class='col divide-left'>" +
@@ -29,8 +29,11 @@ $(document).ready(function(){
                 "</div>" +
             "</div>" +
             "<div class='row'>" +
-                "<div class='col-sm-8 offset-sm-2'>" +
+                "<div class='col-sm-8 offset-sm-2 text-center about-page'>" +
                     "<p class='info'>I am a Full Stack Software Developer based out of Richmond, VA.  When I am not adventuring outdoors, I am developing fully functional web-based applications across several platforms.   I enjoy tackling complex problems that require the utilization of new and evolving technologies in order to create intuitive user interfaces for my clients. My true passion lies in the implementation of appealing design, but I am more than capable of handling everything from the Front End to the Back End.</p>" +
+                    "<button type='button' class='btn btn-dark home'>Home</button>" +
+                    "<button type='button' class='btn btn-dark portfolio'>View My Work</button>" +
+                    "<button type='button' class='btn btn-dark contact'>Get In Touch</button>" +
                 "</div>" +
             "</div>" +
              
@@ -45,28 +48,17 @@ $(document).ready(function(){
         $(".contact-page").fadeIn(1600);
    }
 
-   /* "<div class='row'>" +
-                "<div class ='col-8 offset-2'>" +
-                    "<i class='fab fa-html5 html'></i>" +
-                "</div>" +
-                "<div class='col-8 offset-2'>" +
-                "<i class='fab fa-css3-alt html'></i>" +
-                "</div>" +
-             "</div>" + */
-
-   /* "<div class = 'container'><div class = 'row'><div class = 'col-8 offset-2 col-s-8 offset-s-2 about-main'><div class='row'><div class='col-sm'></div><div class='col-sm'><i class='fab fa-css3-alt html'></i><div class='col-sm></div></div></div><div class='row'></div><button type='button' class='btn btn-dark home'>Home</button><button type='button' class='btn btn-dark portfolio'>View My Work</button><button type='button' class='btn btn-dark contact'>Get In Touch</button></div></div></div>"
-
-   "<div class='container><div class='row'><div class='col-12 img-col'><img src = 'images/twk.jpg' class='about-img'></div></div><div class='row'><div class='col'><i class='fab fa-html5 html'></i></div></div></div>" */
+   
     
 
     $(document).on("click", ".contact", function(){
-        $(".about-main").hide();
+        $(".about-page").hide();
         $(".header").hide();
         populateContact();
     })
 
     $(document).on("click", ".home", function(){
-        $(".about-main").hide();
+        $(".about-page").hide();
         populateMain();
     })
 
