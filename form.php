c4874d696436b13270f9268242632ea280ce4540
Tanner Kirkpatrick <twk4491@gmail.com>
2020-10-11 17:28:45 -0400

    trying to get contact form working

--- form.php
+++ form.php
@@ -3,6 +3,18 @@
 if(isset($_POST['submit'])){
 
     $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+    $mailFrom = $_POST['email'];
+    $message = $_POST['message']; 
+
+    $mailTo = "twk4491@gmail.com";
+
+    $header = "From: ".$mailFrom;
+
+    $txt = "You have received an email from ".$name.".\n\n".$message;
+
+    mail($mailTo, $txt, $header);
+
+    header("Location: index.html?mailsend");
+
+
 }
